89b20d0984091f90920102fe704948525525903e
amtul-nazneen <amtulnazneen7@gmail.com>
2020-09-08 12:34:08 -0500

    lab2 files

---
+++ Lab2_axn180041/src/lab2/app/main/TYPE.java
@@ -0,0 +1,8 @@
+package lab2.app.main;
+
+public class TYPE {
+	protected static final String PERSON = "http://utdallas/semclass#Person";
+	protected static final String BOOK = "http://utdallas/semclass#Book";
+	protected static final String MOVIE = "http://utdallas/semclass#Movie";
+	protected static final String RADIO = "http://utdallas/semclass#Radio";
+}
