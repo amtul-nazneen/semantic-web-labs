ba51c002bf0674f891143912b37ea3791c1c5555
amtul-nazneen <amtulnazneen7@gmail.com>
2020-11-18 19:59:26 -0600

    all assignments

--- Lab2_axn180041/src/lab2/app/main/TYPE.java
+++
@@ -1,8 +0,0 @@
-package lab2.app.main;
-
-public class TYPE {
-	protected static final String PERSON = "http://utdallas/semclass#Person";
-	protected static final String BOOK = "http://utdallas/semclass#Book";
-	protected static final String MOVIE = "http://utdallas/semclass#Movie";
-	protected static final String RADIO = "http://utdallas/semclass#Radio";
-}
